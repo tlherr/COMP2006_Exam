d55a0e340fd601813e1c3a220b876236b0babcae
Thomas Herr <thom1810@gmail.com>
2017-08-14 16:04:24 -0400

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/COMP2006_Exam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/COMP2006_Exam.dir/main.cpp.o"
+  "COMP2006_Exam.pdb"
+  "COMP2006_Exam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/COMP2006_Exam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/COMP2006_Exam.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for COMP2006_Exam.
+# This may be replaced when dependencies are built.
